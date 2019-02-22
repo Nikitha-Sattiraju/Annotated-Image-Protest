fff4c65d28ed119fa3c8c769521cc48fd7f47278
Nikitha Rao <nikitharao@Nikithas-MacBook-Air.local>
2019-02-22 15:58:08 -0500

    modified branch

--- main.js
+++ main.js
@@ -17,3 +17,23 @@ $("body").on("click", ".square .close", function (e) {
 
 	$this_square.removeClass("active");
 });
+
+$("body").on("click", ".box", function (e) {
+	// on click add an active class to the dot that was clicked
+	e.preventDefault();
+
+	const $this_box = $(this);
+
+	$this_box.addClass("active");
+});
+
+$("body").on("click", ".box .close", function (e) {
+	// on click add an active class to the dot that was clicked
+	e.preventDefault();
+	e.stopPropagation();
+
+	const $this_close = $(this);
+	const $this_box = $this_close.closest(".box");
+
+	$this_box.removeClass("active");
+});
